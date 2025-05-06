fdd2d0c0cb4a95e8a965954c53903edada614339
Ismail Lyamani <ismaillyamani4@gmail.com>
2025-05-06 17:13:02 +0100

    interface

--- src/main/java/TP9/database/DatabaseManager.java
+++ src/main/java/TP9/database/DatabaseManager.java
@@ -1,4 +1,13 @@
 package TP9.database;
 
-public class DatabaseManager {
+import java.util.List;
+import java.util.Map;
+
+public interface DatabaseManager {
+    void connect() throws ConnectException;
+    void disconnect() throws ConnectException;
+    List<Map<String, Object>> executeQuery(String sql) throws DQLException;
+    int executeUpdate(String sql) throws DMLException;
+    int executeInsert(String sql) throws DMLException;
+    int executeDelete(String sql) throws DMLException;
 }
